1ff6e621bae8fe05e58973bd92815b43705c35a5
CupidsBow <1143957699@qq.com>
2026-05-04 00:43:48 +0800

    modified param

--- main.py
+++ main.py
@@ -369,9 +369,8 @@ class World:
         if self.tray_icon:
             self.tray_icon.stop()
         self.screen.fill(self.TRANSPARENT_COLOR)
-        self.chat_app.on_closing()
         self.model_manager.archive_chat_range(0, len(self.model_manager.chat_history))
-        self.model_manager.quit_save_memory()
+        # self.model_manager.quit_save_memory()
         pygame.quit()
         sys.exit()
         
